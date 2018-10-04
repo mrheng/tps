0a4d40496893f34ad7ed43e862b36d80172ec0b1
unknown <jobszone@163.com>
2018-10-05 01:24:20 +0800

    zhesho wp de xomxoamg,i

---
+++ application/resume/controller/Index.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\resume\controller;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+use think\Controller;
+/**
+ * Description of Index
+ *
+ * @author gzh
+ */
+class Index extends Controller {
+    public function home(){
+        return $this->fetch();
+    }
+}
